1c32f96797c0560ee7ae05c0a370cc4159ef2760
Eileen Nakahata <esnakahata@Eileens-MacBook-Air.local>
2021-04-10 16:50:01 -0400

    Completed exercise and made changes as directed

--- Exercise-2.R
+++ Exercise-2.R
@@ -7,6 +7,8 @@ ggplot(data = organdata,
   geom_point()
 
 # What does the error message mean here? --> (comment your answer)
+#It often means that there are data points outside the range of the x and y axis. Since we haven't specified range, it 
+#probably means just what it says, that it rows missing a value for year or donors.
 
 # Now let's use geom_line() to plot each country's time series
 ggplot(data = organdata,
@@ -17,7 +19,8 @@ ggplot(data = organdata,
 # Leaving the timeseries aside, we can also look at the number of donors by country:
 ggplot(data = organdata,
        mapping = aes(x = country, y = donors)) + 
-  geom_boxplot()
+  geom_boxplot()+
+  coord_flip()
 
 # This doesn't look great... try adding coord_flip() to the code above.
 
@@ -43,7 +46,8 @@ ggplot(data = organdata,
   geom_point() + 
   labs(x=NULL) + 
   coord_flip() + 
-  theme(legend.position = "top")
+  theme(legend.position = "top")+
+  geom_jitter()
 
 # But these points have some overlapping observations... Try adding geom_jitter() to the plot above. If you don't like the default arguments of geom_jitter, look up at documentation for geom_jitter (https://ggplot2.tidyverse.org/reference/geom_jitter.html) and add additional arguments.
 
@@ -55,15 +59,24 @@ by_country <- organdata %>% group_by(consent_law, country) %>%
 by_country
 
 # What happened inside this pipeline? --> (comment your answer here)
-
+#First it grouped organdata by consent law and country then it used the summarize function to calculate the mean and 
+#standard deviation for all the numeric variables for each country. Finally, it ungrouped the data.
 # Now for the Cleveland dot plot:
 ggplot(data = by_country,
        mapping = aes(x = donors_mean, y = reorder(country, donors_mean),
                      color = consent_law)) + 
   geom_point(size=3) +
+  ggtitle("Highest rates of organ donor procurement in countries with presumed consent laws") +
   labs(x = "Donor Procurement Rate",
        y = "", color = "Consent Law") +
-  theme(legend.position="top")
+  scale_color_manual(values = c("#00AFBB", "#E7B800")) +
+  theme(legend.position="top",
+        panel.background = element_rect(fill="white", linetype="solid", color = "gray"),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) +
+  facet_wrap(~ consent_law)
 
 # Try adding a facet_wrap() by consent law to the plot above. Facet_wrap has additional arguments that you could explore, including scales =, and ncol=. Again, Google is your friend here.
 
