2000c625ba760caff5c5533144bde589bb0f8ec6
Noah Mitchell-Ward <noahmitchellward@NMW-Macbook-Air.local>
2021-04-11 15:18:33 -0400

    Edited the graph to remove gridlines, move the legend, create a faceted graph, change axis labels, and add a bolded title. Also included answers as comments to the questions within the script.

--- Exercise-2.R
+++ Exercise-2.R
@@ -6,7 +6,9 @@ ggplot(data = organdata,
        mapping = aes(x = year, y = donors)) + 
   geom_point()
 
-# What does the error message mean here? --> (comment your answer)
+# What does the error message mean here? --> Several rows have missing values (listed as NA in R), and in
+# order to plot the features we're interested in, the year and donors, we need values in those cells. The 
+# geom_point function has removed those rows that have missing data so they are not plotted.
 
 # Now let's use geom_line() to plot each country's time series
 ggplot(data = organdata,
@@ -17,6 +19,7 @@ ggplot(data = organdata,
 # Leaving the timeseries aside, we can also look at the number of donors by country:
 ggplot(data = organdata,
        mapping = aes(x = country, y = donors)) + 
+  coord_flip() + 
   geom_boxplot()
 
 # This doesn't look great... try adding coord_flip() to the code above.
@@ -43,32 +46,38 @@ ggplot(data = organdata,
   geom_point() + 
   labs(x=NULL) + 
   coord_flip() + 
-  theme(legend.position = "top")
+  theme(legend.position = "top") + 
+  geom_jitter()
 
 # But these points have some overlapping observations... Try adding geom_jitter() to the plot above. If you don't like the default arguments of geom_jitter, look up at documentation for geom_jitter (https://ggplot2.tidyverse.org/reference/geom_jitter.html) and add additional arguments.
 
 # A better altnerative to a jittered plot might be a Cleveland dot plot. Before we can get to a Cleveland dot plot, we'll need to do some summarizing.
 by_country <- organdata %>% group_by(consent_law, country) %>%
-  summarize_if(is.numeric, funs(mean, sd), na.rm = TRUE) %>%
+  summarize_if(is.numeric, list(mean = mean, sd = sd), na.rm = TRUE) %>% #the 'funs' function was deprecated so I used a list of the stats we are looking for
   ungroup()
 
 by_country
 
-# What happened inside this pipeline? --> (comment your answer here)
+# What happened inside this pipeline? --> This pipeline takes the organ donor data and reorganizes it by country, with the consent type being the first column.
+# This means that instead of individual observations, each country's stats are summarized by mean and standard deviation.
+# First, we had to group by both consent law and country to get the summary stats, then ungroup at the end to get the table we want.
 
 # Now for the Cleveland dot plot:
 ggplot(data = by_country,
        mapping = aes(x = donors_mean, y = reorder(country, donors_mean),
                      color = consent_law)) + 
   geom_point(size=3) +
-  labs(x = "Donor Procurement Rate",
+  labs(x = "Donor Procurement Rate (%)", # added unit of percentage to give additional clarity
        y = "", color = "Consent Law") +
-  theme(legend.position="top")
-
+  theme(legend.position="bottom", plot.title = element_text(face = "bold")) + # moved the legend to the bottom and bolded the title
+  facet_wrap(~ consent_law, scales ="free_x") + # changed the x-axis scale to be reactive to the range of points in each plot
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + # removed gridlines
+  ggtitle("Organ donor procurement rates are greater\nin countries with presumed consent", subtitle = "") # added a title and added a newline character because title was getting cutoff
+  
 # Try adding a facet_wrap() by consent law to the plot above. Facet_wrap has additional arguments that you could explore, including scales =, and ncol=. Again, Google is your friend here.
 
 # Finally, add a title and remove gridlines. Once you are happy with your final Cleveland dot plot, save it.
 
-ggsave("plot2.png",
+ggsave("NMW HW9 Exercise 2 Plot.png",
        plot = last_plot(),
-       dpi = 300)
+       dpi = 500)
