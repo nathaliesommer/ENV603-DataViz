d8a3f5cd8f94235a1165d1701ae9707dfb34b1e7
Gabrilyan <svetlana.gabrilyan@yale.edu>
2021-04-11 16:20:48 -0400

    completed assignment! modified the code for plot p2. also saved it as plot1

--- Exercise-1.R
+++ Exercise-1.R
@@ -1,22 +1,30 @@
-#### ENV 603 / 5-April-2021 / N.R. Sommer
+#### ENV 603 / 11-April-2021 / Lana Gabrilyan
 # Dataset 1: Religion by region
 
 # If you have not yet installed these libraries, use install.packages("")
 
 library(tidyverse)
 library(socviz)
+library(ggplot2)
+data("gss_sm")
+data1 <- gss_sm
 
+# Install
+install.packages("viridis")
+# Load
+library(viridis)
 # Create a new table called rel_by_region
 
-rel_by_region <- gss_sum %>%
+rel_by_region <- gss_sm %>%
   group_by(bigregion, religion) %>%
   summarize(N = n()) %>%
   mutate(freq = N / sum(N),
          pct = round((freq*100), 0))
 
+
 # See how the pipeline above has taked the gss_sm dataframe and transformed it into a summary table.
 
-View(gss_sum)
+View(gss_sm)
 View(rel_by_region)
 
 # Now let's make some plots!
@@ -28,12 +36,14 @@ p1 <- ggplot(rel_by_region, aes(x = bigregion, y = pct, fill = religion)) +
 
 p1
 
-p2 <- ggplot(rel_by_region, aes(x = religion, y = pct, fill = religion)) +
+p2 <- ggplot(rel_by_region, aes(x = reorder(religion, pct), y = pct, fill = religion)) +
   geom_col(position = "dodge2") +
   labs(x = NULL, y = "Percent", fill = "Religion") +
   guides(fill = FALSE) + 
   coord_flip() + 
-  facet_grid(~ bigregion)
+  facet_grid(~ bigregion) + ggtitle("Percentage of religious people in different regions") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
+  scale_fill_viridis_d()
 
 p2
 
