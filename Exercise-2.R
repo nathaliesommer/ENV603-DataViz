b1aa07edcb2cad976a74e4cd78c278f385d0ff93
lerdy59 <lerdy59@gmail.com>
2021-04-11 21:55:14 -0400

    Cleveland dot plot for Exercise 2

--- Exercise-2.R
+++ Exercise-2.R
@@ -7,6 +7,7 @@ ggplot(data = organdata,
   geom_point()
 
 # What does the error message mean here? --> (comment your answer)
+#the error message means the plot we just made excluded the rows that have missing value which shows as NA 
 
 # Now let's use geom_line() to plot each country's time series
 ggplot(data = organdata,
@@ -41,6 +42,7 @@ ggplot(data = organdata,
        mapping = aes(x = reorder(country, donors, na.rm=TRUE),
                      y = donors, color = world)) +
   geom_point() + 
+  geom_jitter() +
   labs(x=NULL) + 
   coord_flip() + 
   theme(legend.position = "top")
@@ -55,20 +57,32 @@ by_country <- organdata %>% group_by(consent_law, country) %>%
 by_country
 
 # What happened inside this pipeline? --> (comment your answer here)
+# In this pipeline, the previous dataset are grouped by countries. Instead of showing the annual values for each country, the pipeline calculated the mean and standard deviation for each country. 
 
 # Now for the Cleveland dot plot:
 ggplot(data = by_country,
-       mapping = aes(x = donors_mean, y = reorder(country, donors_mean),
-                     color = consent_law)) + 
+       mapping = aes(x = donors_mean, y = reorder(country, donors_mean))) + 
   geom_point(size=3) +
   labs(x = "Donor Procurement Rate",
-       y = "", color = "Consent Law") +
-  theme(legend.position="top")
+       y = "") +
+  theme(legend.position="top") + 
+  facet_wrap("consent_law") +  
+  ggtitle("Average Organ Donor Procurement Rate in 17 OECD Countries from 1991 to 2002",subtitle =  "Countries with a model of presumed consent usually have higher procurement rate than others") + 
+  theme(plot.subtitle = element_text(colour="gray40"), #change subtitle font 
+        axis.text.y = element_text(size = 11, colour="black"),
+        axis.ticks.y = element_blank(),
+        axis.line.y = element_line(size = 0.55, colour ="darkgray"),
+        axis.line.x = element_line(size = 0.55, colour ="darkgray"),
+        panel.grid.major = element_blank(), #remove grids
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), #remove background color
+        plot.margin = unit(c(1,1,1,1), "cm"),
+        panel.spacing.x = unit(2, "lines")) + #add spaces between each facet so we can see the entire axis 
+
 
 # Try adding a facet_wrap() by consent law to the plot above. Facet_wrap has additional arguments that you could explore, including scales =, and ncol=. Again, Google is your friend here.
 
 # Finally, add a title and remove gridlines. Once you are happy with your final Cleveland dot plot, save it.
-
-ggsave("plot2.png",
-       plot = last_plot(),
-       dpi = 300)
+  ggsave("Exercise2_Cleveland.png",
+         plot = last_plot(),
+         height=8, width=12)
