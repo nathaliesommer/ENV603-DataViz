b53f2908fd49a43ea7ea41fe9d8407eba81fcc5c
Aymane Eddahmani <aymane.eddahmani@yale.edu>
2021-04-11 22:06:43 -0400

    - Cleaned Code - Reordered levels - Cleaned and saved P1:    - changed color    - reordered graphs    - removed grid lines (except y)    - added title    - moved legend

--- Exercise-1.R
+++ Exercise-1.R
@@ -4,6 +4,8 @@
 
 library(tidyverse)
 library(socviz)
+library(RColorBrewer)
+library(viridis)
 
 # Create a new table called rel_by_region
 rel_by_region <- gss_sm %>%
@@ -17,14 +19,24 @@ rel_by_region <- gss_sm %>%
 View(gss_sm)
 View(rel_by_region)
 
-# Now let's make some plots! blabla
+# Now let's make some plots!
 
-p1 <- ggplot(rel_by_region, aes(x = bigregion, y = pct, fill = religion)) + 
+
+
+#reordering the levels:
+rel_by_region$religion <- factor(rel_by_region$religion, levels = c("Protestant", "Catholic", "None", "Jewish","Other"))
+
+#changed the color scale. grid lines, and general formatting 
+p1 <- rel_by_region %>% filter(!is.na(religion))  %>%  ggplot(aes(x = bigregion, y = pct, fill = religion)) + 
   geom_col(position = "dodge2") +
-  labs(x = "Region",y = "Percent", fill = "Religion") +
-  theme(legend.position = "top")
-
+  theme_minimal()+
+  labs(x = "Region",y = "Percentage", fill = "Religion", title = "Most Americans in all major US regions affiliate with a religion") +
+  theme(legend.position = "right",panel.grid.major.x = element_blank())+
+  scale_fill_viridis(discrete= TRUE)
 p1
+ggsave("plot1.png",
+       plot = last_plot(),
+       dpi = 300)
 
 p2 <- ggplot(rel_by_region, aes(x = religion, y = pct, fill = religion)) +
   geom_col(position = "dodge2") +
@@ -34,14 +46,3 @@ p2 <- ggplot(rel_by_region, aes(x = religion, y = pct, fill = religion)) +
   facet_grid(~ bigregion)
 
 p2
-
-# Make modifications to either plot. Google is your friend here. A few suggestions:
-# (1) Add a title
-# (2) Remove the gridlines
-# (3) Reorder the bars
-# (4) Choose a new color scheme
-
-# Once you're happy with your changes, save your plot:
-ggsave("plot1.png",
-  plot = last_plot(),
-  dpi = 300)
